63303b35bfa1d741f260f888bdfe67f238616608
Himank Goel <himankgoel03@gmail.com>
2021-08-28 21:23:05 +0530

    added checkpoints and trained model

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,7 +1,9 @@
-from tensorflow.keras.layers import (ELU, Activation, BatchNormalization,
-                                     Conv2D, Dense, Dropout, Flatten,
-                                     MaxPooling2D)
+from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint,
+                                        ReduceLROnPlateau)
+from tensorflow.keras.layers import (Activation, BatchNormalization, Conv2D,
+                                     Dense, Dropout, Flatten, MaxPooling2D)
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 num_classes = 6
@@ -41,19 +43,8 @@ validation_generator = validation_datagen.flow_from_directory(
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding = 'same', kernel_initializer="he_normal", 
+model.add(Conv2D(64, (3, 3), padding = 'same', kernel_initializer="he_normal", 
                  input_shape = (img_rows, img_cols, 1)))
-model.add(Activation('elu'))
-model.add(BatchNormalization())
-
-model.add(Conv2D(32, (3, 3), padding = "same", kernel_initializer="he_normal"))
-model.add(Activation('elu'))
-model.add(BatchNormalization())
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(64, (3, 3), padding = 'same', kernel_initializer="he_normal"))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 
@@ -86,6 +77,28 @@ model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
+model.add(Conv2D(512, (3, 3), padding = 'same', kernel_initializer="he_normal"))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+
+model.add(Conv2D(512, (3, 3), padding = "same", kernel_initializer="he_normal"))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(1024, (3, 3), padding = 'same', kernel_initializer="he_normal"))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+
+model.add(Conv2D(1024, (3, 3), padding = "same", kernel_initializer="he_normal"))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
 model.add(Flatten())
 model.add(Dense(64, kernel_initializer="he_normal"))
 model.add(Activation('elu'))
@@ -102,3 +115,34 @@ model.add(Activation("softmax"))
 
 print(model.summary())
 
+checkpoint = ModelCheckpoint("emotion_little_vgg.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 3, verbose = 1, min_delta = 0.0001)
+
+callbacks = [earlystop, checkpoint, reduce_lr]
+
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = Adam(learning_rate=0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 28273
+nb_validation_samples = 3534
+epochs = 25
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
